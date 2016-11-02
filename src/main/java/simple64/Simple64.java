e42316670ca89d49423086fe0700a6938dc84ecd
Stefan Rieken <stefan.rieken@42.nl>
2016-11-02 09:59:13 +0100

    get executable from args

--- src/main/java/simple64/Simple64.java
+++ src/main/java/simple64/Simple64.java
@@ -1,5 +1,6 @@
 package simple64;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -16,32 +17,35 @@ public class Simple64 {
 		mem.peripherals.add(new Keyboard());
 		
 		Simple64 s = new Simple64();
-		s.load(mem);
+		s.load(mem, args[0]);
+		
+		// The ROM reset vector
+		mem.set(0xFFFC, (short) 0x00);
+		mem.set(0xFFFD, (short) 0x06);
+
 		s.run(mem);
 	}
 	
-	private void load(Memory mem) {
-		InputStream stream = getClass().getResourceAsStream("/compiled/ttvm.o65");
-		
+	private void load(Memory mem, String file) {
 		int start = 0x0600;
 
 		try {
+			InputStream stream = new FileInputStream(file);
 			
-			int ch = stream.read();
-			int i=0;
-			while (ch != -1) {
-				mem.set(start+i, (short) ch);
-			
-				ch = stream.read();
-				i++;
+			try {
+				int ch = stream.read();
+				int i=0;
+				while (ch != -1) {
+					mem.set(start+i, (short) ch);
+				
+					ch = stream.read();
+					i++;
+				}
+			} finally {
+				stream.close();
 			}
 
-			stream.close();
 			
-			// The ROM reset vector
-			mem.set(0xFFFC, (short) 0x00);
-			mem.set(0xFFFD, (short) 0x06);
-
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -56,13 +60,15 @@ public class Simple64 {
 		p.sp = 0xFF;
 
 		p.reset();
-		
-		long time = System.nanoTime();
-		long timePassed;
+	
+		while (p.run()) {};
 
-		while(p.run()) {
-			do { timePassed = System.nanoTime() - time; } while (timePassed < 50000);
-			time = System.nanoTime();
-		}
+//		long time = System.nanoTime();
+//		long timePassed;
+//
+//		while(p.run()) {
+//			do { timePassed = System.nanoTime() - time; } while (timePassed < 50000);
+//			time = System.nanoTime();
+//		}
 	}
 }
---
+++ src/test/java/simple64/Simple64Test.java
@@ -0,0 +1,11 @@
+package simple64;
+
+import org.junit.Test;
+
+public class Simple64Test {
+
+	@Test
+	public void run() {
+		new Simple64().main("src/test/resources/compiled/colors.o65");
+	}
+}
